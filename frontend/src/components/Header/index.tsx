b4351bedeeb2a02e48fccf483d0a81fba200c193
ta-yabiku <ta-yabiku@kentem.co.jp>
2025-06-23 08:55:12 +0900

    change: アプリ名変更

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,10 +1,12 @@
-import styles from './index.module.scss';
+import styles from "./index.module.scss";
 
 const Header = () => {
   return (
-    <header className={styles['header-container']}>
-      <h1 className={styles['title']}>Maiby Tipping</h1>
-      <p className={styles['subtitle']}>そんな気にせず、70%でもいいじゃないか</p>
+    <header className={styles["header-container"]}>
+      <h1 className={styles["title"]}>Maibe Typeing</h1>
+      <p className={styles["subtitle"]}>
+        そんな気にせず、70%でもいいじゃないか
+      </p>
     </header>
   );
 };
